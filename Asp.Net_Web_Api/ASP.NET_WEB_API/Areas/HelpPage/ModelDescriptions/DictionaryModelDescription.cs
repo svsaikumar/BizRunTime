42ab52de5e6a61516c7c2bcea0dce9aa5af6d160
svsaikumar <saikumarsv77@gmail.com>
2018-08-08 20:38:02 +0530

    asp.net

---
+++ Asp.Net_Web_Api/ASP.NET_WEB_API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ASP.NET_WEB_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Asp.Net_Web_Api/ASP.NET_WEB_API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_WEB_API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
